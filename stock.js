1ca8761206dd2ee8b0f52ca90d2a3020b490d23e
vagrant <albert.cherng@gmail.com>
2016-07-08 02:13:31 +0000

    Add stock exercise

--- stock.js
+++ stock.js
@@ -1,11 +1,20 @@
 function maxFunction(arr){
-  var minNum = arr[0];
-  for (number of arr) {
-    if (number < minNum) {
-      minNum = number;
+  var minPrice = arr[0];
+  var maxProfit = 0
+  for (i = 0; i < arr.length; i++) {
+    var price = arr[i];
+    if (price < minPrice) {
+      minPrice = price;
     }
-  }
-console.log(minNum);
+    if (maxProfit < price - minPrice) {
+      maxProfit = (price - minPrice);
+    }
+  };
+  console.log(maxProfit);
 }
 
+
+
 maxFunction([45, 24, 35, 31, 40, 38, 11])
+maxFunction([30, 48, 103, 23, 13, 3 ,32])
+maxFunction([3320, 3, 43434343, 23, 13, 3 ,32])
